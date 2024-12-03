6e6b7f4ea0a160650b29044ba409e0c5f2c55c2c
Félix Bastías <129023382+BlueBoxNora@users.noreply.github.com>
2024-12-02 22:14:09 -0300

    Update Carro.js

--- appproductos/src/Carro.js
+++ appproductos/src/Carro.js
@@ -46,7 +46,7 @@ function Carro() {
               return <div class="product-card" style={{ 'padding':'10px','border':'2px solid rgb(13, 77, 77)''}}>
                 
                 
-              <div class="product-image" style={{'height':'auto','width':'auto', 'padding':'10px'}}>
+              <div class="product-image" style={{'height':200,'width':200, 'padding':'10px'}}>
                 <img class="foto" src={getProductos.foto} />
               </div>
               <div class="product-info" style= {{'padding-top':'12rem'}}>
